8cc95bc2bc860bc3bc297438e7ede146bc75c19a
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2019-09-23 18:04:13 +0530

    added support for command line arguments in getter and setter of work request and specs

--- fabcar/javascript/setRequest.js
+++ fabcar/javascript/setRequest.js
@@ -39,7 +39,7 @@ async function main() {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('CreateWorkRequest', JSON.stringify(obj));
+        await contract.submitTransaction('CreateWorkRequest', process.argv[2]);
         console.log('Transaction has been submitted');
 
         // Disconnect from the gateway.
